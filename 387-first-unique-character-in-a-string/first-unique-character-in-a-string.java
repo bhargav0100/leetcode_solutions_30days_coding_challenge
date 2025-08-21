572334a15537141a8a3751050ca0bd83910d08bf
bhargav0100 <155953883+bhargav0100@users.noreply.github.com>
2025-08-21 18:45:35 +0530

    Time: 14 ms (54.93%) | Memory: 45.3 MB (47.71%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,28 +1,30 @@
 class Solution {
     public int firstUniqChar(String s) {
-        Map<Character,Integer>m=new HashMap();
-        char[]ch=s.toCharArray();
-        for(char c:ch)
+        if(s.length()==1) return 0;
+        Set<Character>set=new HashSet();
+        for(int i=0;i<s.length()-1;i++)
         {
-            if(!m.isEmpty() && m.containsKey(c))
+            int freq=0;
+            if(set.isEmpty() || !set.contains(s.charAt(i)))
             {
-                int i=m.get(c);
-                m.put(c,i+1);
-            }
-            else
-            {
-                m.put(c,0);
-            }
-        }
-        for(int i=0;i<ch.length;i++)
-        {
-            if(m.containsKey(ch[i]))
-            {
-                if(m.get(ch[i])==0)
+                for(int j=i+1;j<s.length();j++)
+                {
+                    if(s.charAt(i)==s.charAt(j))
+                    {
+                        freq++;
+                        break;
+                    }
+                }
+                if(freq==0)
                 {
                     return i;
                 }
+                set.add(s.charAt(i));
             }
+        }
+        if(!set.contains(s.charAt(s.length()-1)))
+        {
+            return s.length()-1;
         }
         return -1;
     }
