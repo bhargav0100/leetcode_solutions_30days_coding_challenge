61b9e867592837b379eb48111a421aad21130dae
bhargav0100 <155953883+bhargav0100@users.noreply.github.com>
2025-08-22 11:19:44 +0530

    Time: 5 ms (54.59%) | Memory: 45.5 MB (6.24%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -8,20 +8,12 @@ class Solution {
         m.put('C',100);
         m.put('D',500);
         m.put('M',1000);
-        int num=0;
-        for(int i=0;i<s.length();i++)
+        int num=m.get(s.charAt(s.length()-1));
+        for(int i=s.length()-2;i>=0;i--)
         {
-            if(i>0 && (s.charAt(i)=='X' || s.charAt(i)=='V') && s.charAt(i-1)=='I')
+            if(m.get(s.charAt(i))<m.get(s.charAt(i+1)))
             {
-                num+=m.get(s.charAt(i))-2;
-            }
-            else if(i>0 && (s.charAt(i)=='L' || s.charAt(i)=='C') && s.charAt(i-1)=='X')
-            {
-                num+=m.get(s.charAt(i))-20;
-            }
-            else if(i>0 && (s.charAt(i)=='D'||s.charAt(i)=='M') && s.charAt(i-1)=='C')
-            {
-                num+=m.get(s.charAt(i))-200;
+                num-=m.get(s.charAt(i));
             }
             else
             {
