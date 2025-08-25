3b71a2f0a40874f90b5f369c0f0167254699dbdf
bhargav0100 <155953883+bhargav0100@users.noreply.github.com>
2025-08-25 14:08:03 +0530

    Time: 1 ms (20.96%) | Memory: 41.8 MB (82.95%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        Map<Integer,Integer>m=new TreeMap();
+        m.put(0,0);
+        m.put(1,0);
+        m.put(2,0);
+        for(int n:nums)
+        {
+            if(m.containsKey(n))
+            {
+                int i=m.get(n);
+                m.put(n,i+1);
+            }
+        }
+        int i=0;
+        int k=0;
+        while(i<3)
+        {
+            for(int j=0;j<m.get(i);j++)
+            {
+                nums[k++]=i;
+            }
+            i++;
+        }
+    }
+}
