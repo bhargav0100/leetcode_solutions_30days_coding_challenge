259df08b639875b761c08d3db2e8a249042c389e
bhargav0100 <155953883+bhargav0100@users.noreply.github.com>
2025-08-29 13:04:59 +0530

    Time: 5 ms (20.85%) | Memory: 45.3 MB (92.30%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -0,0 +1,55 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i=0;
+        int j=s.length()-1;
+        if(palindromeRemoveLeft(s,i,j))
+        {
+            return true;
+        }
+        else return palindromeRemoveRight(s,i,j);
+    }
+    private boolean palindromeRemoveLeft(String s,int i,int j)
+    {
+        int count=0;
+        while(i<=j)
+        {
+            if(s.charAt(i)!=s.charAt(j))
+            {
+                if(s.charAt(i+1)==s.charAt(j) && count!=1)
+                {
+                    count++;
+                    i++;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    private boolean palindromeRemoveRight(String s,int i,int j)
+    {
+        int count=0;
+        while(i<=j)
+        {
+            if(s.charAt(i)!=s.charAt(j))
+            {
+                if(s.charAt(i)==s.charAt(j-1) && count!=1)
+                {
+                    count++;
+                    j--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
