019d500b4a93a4237b3e02570f769586c851ae4d
bhargav0100 <155953883+bhargav0100@users.noreply.github.com>
2025-08-31 13:55:44 +0530

    Time: 1231 ms (5.00%) | Memory: 40.4 MB (67.76%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num==1) return true;
+        for(int i=2;i<num;i++)
+        {
+            if(i*i==num)
+            {
+                return true;
+            }
+            if(i*i>num)
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+}
