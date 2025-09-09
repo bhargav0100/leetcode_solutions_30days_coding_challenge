40439147253747d76b9f45a23aa946497fdd9e91
bhargav0100 <155953883+bhargav0100@users.noreply.github.com>
2025-09-09 19:28:52 +0530

    Time: 2 ms (30.12%) | Memory: 43.7 MB (30.72%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,18 +1,17 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>>subsetList=new ArrayList();
+        Set<List<Integer>>subsetList=new HashSet();
         Arrays.sort(nums);
         backTracking(subsetList,new ArrayList(),nums,0);
-        return subsetList;
+        return new ArrayList(subsetList);
     }
-    public void backTracking(List<List<Integer>>subsetList,List<Integer>subset,int[] nums,int start)
+    public void backTracking(Set<List<Integer>>subsetList,List<Integer>subset,int[] nums,int start)
     {
-        if(subsetList.contains(subset)) return ;
-        subsetList.add(new ArrayList(subset));
+        subsetList.add(subset);
         for(int i=start;i<nums.length;i++)
         {
             subset.add(nums[i]);
-            backTracking(subsetList,subset,nums,i+1);
+            backTracking(subsetList,new ArrayList(subset),nums,i+1);
             subset.remove(subset.size()-1);
         }
     }
