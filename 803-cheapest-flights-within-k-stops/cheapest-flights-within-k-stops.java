3300dd2a15a02b4eb8d68b58846dc23e285bdd06
bhargav0100 <155953883+bhargav0100@users.noreply.github.com>
2025-09-28 14:21:35 +0530

    Time: 7 ms (42.94%) | Memory: 44.1 MB (93.65%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -18,33 +18,29 @@ class Solution {
         q.add(Arrays.asList(0,src,0));
         while(!q.isEmpty())
         {
-            
-                int stops=q.peek().get(0);
-                if(stops>k) break;
-                int node=q.peek().get(1);
-                int cost=q.peek().get(2);
-                q.remove();
-                if(cost>=min[dst]) continue;
-                List<List<Integer>> temp=pair.get(node);
-                if(temp!=null)
+            int stops=q.peek().get(0);
+            if(stops>k) break;
+            int node=q.peek().get(1);
+            int cost=q.peek().get(2);
+            q.remove();
+            if(cost>=min[dst]) continue;
+            List<List<Integer>> temp=pair.get(node);
+            for(List<Integer> t:temp)
+            {
+                int tnode=t.get(0);
+                int tcost=t.get(1);
+                if(min[tnode]>cost+tcost)
                 {
-                    for(List<Integer> t:temp)
-                    {
-                        int tnode=t.get(0);
-                        int tcost=t.get(1);
-                        if(min[tnode]>cost+tcost)
-                        {
-                            min[tnode]=Math.min(min[tnode],cost+tcost);
-                        }
-                        else
-                        {
-                            continue;
-                        }
-                        if(min[dst]<=cost+tcost) continue;
-                        if(tnode==dst || pair.get(tnode)==null) continue;
-                        q.add(Arrays.asList(stops+1,tnode,tcost+cost));
-                    }
+                    min[tnode]=Math.min(min[tnode],cost+tcost);
                 }
+                else
+                {
+                    continue;
+                }
+                if(min[dst]<=cost+tcost) continue;
+                if(tnode==dst || pair.get(tnode)==null) continue;
+                q.add(Arrays.asList(stops+1,tnode,tcost+cost));
+            }
         }
         if(min[dst]==Integer.MAX_VALUE) return -1;
         return min[dst];
